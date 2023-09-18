cbc1519754a61c0bb891ed3ccd300ff4896b7dee
tgn <alexanderwebber76@gmail.com>
2023-09-17 19:38:57 -0700

    built branch component

--- react-client/src/components/MessageDisplay.tsx
+++ react-client/src/components/MessageDisplay.tsx
@@ -1,4 +1,5 @@
 import { Message } from '../types/main';
+import Branch from './Branch';
 
 type Props = {
     message: Message
@@ -8,6 +9,7 @@ type Props = {
 function MessageDisplay({message, index}: Props) {
   return (
     <div className="group mb-2 shrink-0 rounded-lg flex items-center drop-shadow-md bg-zinc-700 w-48 h-20 relative">
+      <Branch trunk={index === 0} />
       <p className="text-sm text-gray-200 font-bold px-4">
         {message.body}
       </p>
