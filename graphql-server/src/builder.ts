0919b0318f1b7c04eb45ee54291b830c1b0676e8
tgn <alexanderwebber76@gmail.com>
2023-09-17 21:31:53 -0700

    link db via prisma

--- graphql-server/src/builder.ts
+++ graphql-server/src/builder.ts
@@ -1,10 +1,18 @@
 import SchemaBuilder from '@pothos/core';
 import { DateResolver } from 'graphql-scalars';
-
+import PrismaPlugin from "@pothos/plugin-prisma";
+import { prisma } from './db'
+import type PrismaTypes from "@pothos/plugin-prisma/generated";
 export const builder = new SchemaBuilder<{
     Scalars: {
         Date: { Input: Date; Output: Date };
+    };
+    PrismaTypes: PrismaTypes;
+}>({
+    plugins: [PrismaPlugin],
+    prisma: {
+        client: prisma,
     }
-}>({});
+});
 
 builder.addScalarType("Date", DateResolver, {});
